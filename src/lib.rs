5fe760eb5c2d2e35da5bd06913561b782e61dfd4
Peter Schilling <git@schpet.com>
2025-02-15 16:05:52 -0800

    refactor: Simplify changelog version header format

--- src/lib.rs
+++ src/lib.rs
@@ -984,7 +984,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
     fn test_changelog_format_with_git_range_url() {
         let input = r#"# Changelog
 
-## [0.6.11] - 2025-01-19
+## 0.6.11 - 2025-01-19
 
 ### CLI
 
